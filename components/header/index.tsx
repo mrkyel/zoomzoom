197d7a7ec1aa45a2b9a839e6e9b50ca4f0c30271
wishbeenkale <kale@wishbeen.com>
2023-06-03 10:14:48 +0900

    헤더 스크롤 수정, 위시카운트 수정, 반응형 모바일 기준수정

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./Header.module.scss";
 import HeartIcon from "../../public/heart.svg";
 import { useAtom } from "jotai";
@@ -7,13 +7,32 @@ import { heartCountAtom } from "./../../atoms/like";
 
 function Header() {
   const [heartCount] = useAtom(heartCountAtom);
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
+  const [visible, setVisible] = useState(true);
+
+  const formattedHeartCount = heartCount >= 10 ? "9+" : heartCount;
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollPos = window.pageYOffset;
+      const isScrollingDown = currentScrollPos > prevScrollPos;
+
+      setVisible(!isScrollingDown);
+      setPrevScrollPos(currentScrollPos);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [prevScrollPos]);
 
   return (
-    <header className={styles.header}>
+    <header className={`${styles.header} ${visible ? "" : styles.hidden}`}>
       <Image src="/zoomzoomtour-logo.png" alt="logo" width={75} height={50} />
       <div className={styles["heart-container"]}>
         <HeartIcon style={{ width: "25px", height: "25px" }} />
-        <div className={styles["heart-count"]}>{heartCount}</div>
+        <div className={styles["heart-count"]}>{formattedHeartCount}</div>
       </div>
     </header>
   );
